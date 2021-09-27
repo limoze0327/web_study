bcef2dd9b4daa00ff92eece04f203b1c5a57892c
limoze0327 <81509483+limoze0327@users.noreply.github.com>
2021-09-27 14:58:56 +0900

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,14 @@
+<meta charset="utf-8">
+<!DOCTYPE html>
+
+<html>
+<head>
+  <title>사이트에서 로그아웃 합니다.</title>
+</head>
+<body>
+<br>
+<?php
+include "/register.php";
+session_destroy();
+header('Location: join.html');
+?>
