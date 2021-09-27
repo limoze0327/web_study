8826c3005511a16a470c92579b9c4c02bc2c72a7
limoze0327 <81509483+limoze0327@users.noreply.github.com>
2021-09-27 15:00:57 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+$servername="localhost";
+$username="root";
+$password="123456";
+$database="web_db";
+$con=mysqli_connect($servername,$username,$password,$database)
+      or die("Mysql Connect Error!");
+
+if (isset($_SESSION['userID'])){ echo "<br>"; }
+else{ Header('location: login.php'); }
+?>
+
+<meta charset="utf-8">
+<!DOCTYPE html>
+<html>
+<head>
+  <title>인덱스 페이지</title>
+</head>
+
+<body>
+  <h1>
+    INDEX
+  </h1>
+
+  <?php
+  $sel_query="SELECT * FROM member WHERE id LIKE '{$_SESSION['userID']}';";
+  $sel_result=mysqli_query($con, $sel_query) or die("Query Error!");
+
+  $record=mysqli_ferch_array($sel_result);
+
+  echo "<br>
+  <b>{$record['name']}의 페이지입니다</b> <br>
+  login ID : {$record['id']} <br>
+  login PW : {$record['pw']} <br>
+  your email is {$record['email']} <br>
+  your name is {$record['name']} <br>
+  your sex is {$record['sex']} <br> <br>";
+  ?>
+</body>
+</html>
